73cfd1ce781a2faff16422ed5ab5c95e9bcb8d6b
Oksana Serdiuk <okserdiuk@gmail.com>
2017-03-08 11:04:55 +0200

    added models

---
+++ Travelars.DTO/Route/RouteOrientation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travelars.DTO.Route
+{
+    public enum RouteOrientation
+    {
+        Child,
+        Teenager,
+        Couple,
+        Mature
+    }
+}
---
+++ Travelars/Models/Route/RouteRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Travelars.Models.Route
+{
+    public class RouteRequest
+    {
+        public int NumberOfTravelers { get; set; }
+
+        public RouteOrientation RouteOrientation { get; set; }
+
+        public bo Type { get; set; }
+    }
+}
